4a39b98eb56605eccdaaad846af3d2acbce8634a
Jake <jakelovitz@gmail.com>
2019-07-11 15:43:31 -0400

    finished Redux Create Store lab

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,37 @@
+// import candyReducer from './reducers/candyReducer.js'
+// import countReducer from './reducers/countReducer.js'
+// import { create } from 'domain';
+
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  };
+  
+  dispatch({ type: '@@INIT' })
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+
+
+// let storeCandy = createStore(candyReducer);
+// let storeCount = createStore(countReducer);
+
+// reducer(undefined, { type: 'INIT' })
+
+//this breaks the tests
+// storeCandy.dispatch({ type: '@@INIT' });
